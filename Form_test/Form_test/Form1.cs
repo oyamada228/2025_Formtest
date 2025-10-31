a5375725bd72c8855ec73662efb1cae74990f555
oyamada <hatekima228@gmail.com>
2025-10-31 15:56:16 +0900

    a

--- Form_test/Form_test/Form1.cs
+++ Form_test/Form_test/Form1.cs
@@ -11,25 +11,44 @@ using System.Windows.Forms;
 namespace Form_test
 {
     public partial class Form1 : Form
-    {
+    { 
+        //const をつけると初期化時にのみ値の変更可能になる
+        const int BUTTON_SIZE_X = 100;  //この関数がそのまま適応される
+        const int BUTTON_SIZE_Y = 100;  //この関数がそのまま適応される
+
+        const int BOARD_SIZE_X = 3;
+        const int BOARD_SIZE_Y = 3;
+
+        private TestButton[,] _buttonArray;
+
+
         public Form1()
         {
             InitializeComponent();
+            _buttonArray = new TestButton[BOARD_SIZE_Y, BOARD_SIZE_X];
 
-            for (int i = 0; i < 6; i++)
+            for (int i = 0; i < 3; i++)
             {
-                for (int j = 0; j < 4; j++)
+                for (int j = 0; j < 3; j++)
                 {
-                    TestButton testButton = new TestButton(new Point(50 * i, 50 * j),new Size(50, 50),"あ");  //インスタンスの生成
+                   //インスタンスの生成
+                  TestButton testButton =
+                  new TestButton(new Point(BUTTON_SIZE_X  * i, BUTTON_SIZE_Y * j),
+                  new Size(BUTTON_SIZE_X, BUTTON_SIZE_Y)," "); 
 
+                //配列にボタンの参照 
+                 _buttonArray[j, i] = testButton;
 
-                    Controls.Add(testButton);   //コントールにボタン追加
+                   Controls.Add(testButton);   //コントールにボタン追加
                 }
             }
 
+            _buttonArray[0, 0]. SetEnable(true);
+            _buttonArray[1, 0].SetEnable(true);
+            _buttonArray[2, 0].SetEnable(true);
+
 
         }
-
 
         private void ClickEvent(object sender, EventArgs e)
         {
--- Form_test/Form_test/testbutton.cs
+++ Form_test/Form_test/testbutton.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
@@ -11,13 +12,33 @@ namespace Form_test
 {
     internal class TestButton : Button
     {
+        private Color _onColor = Color.White; //onの時の色
+        private Color _offColor = Color.Black;      // offの時の色
+        private bool  _enable; 
+
+        public void SetEnable(bool on)
+        {
+            _enable = on;
+            if (on)
+            {
+                BackColor = _onColor;
+            }
+            else
+            {
+
+                BackColor = _offColor;
+            }
+
+        }
+
         public TestButton(Point position, Size size, string  text)
         {
 
-            Location = position;
-            Size = size;
-            Text = text;
+            Location = position; //位置
+            Size = size;　　　　//大きさ
+            Text = text;        //テキスト
 
+            SetEnable(false);
 
             Click += ClickEvent;
         }
@@ -25,7 +46,7 @@ namespace Form_test
 
         private void ClickEvent(object sender, EventArgs e)
         {
-            MessageBox.Show("こんにちは");
+            SetEnable(!_enable);
         }
 
     }
