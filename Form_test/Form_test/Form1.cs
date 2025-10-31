93841a9e21c04b8e52bc4f532dd9f81490a15e16
oyamada <hatekima228@gmail.com>
2025-10-31 14:34:24 +0900

    a

---
+++ Form_test/Form_test/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    TestButton testButton = new TestButton(new Point(50 * i, 50 * j),new Size(50, 50),"あ");  //インスタンスの生成
+
+
+                    Controls.Add(testButton);   //コントールにボタン追加
+                }
+            }
+
+
+        }
+
+
+        private void ClickEvent(object sender, EventArgs e)
+        {
+            MessageBox.Show("こんにちは");
+        }
+
+
+    }
+}
---
+++ Form_test/Form_test/testbutton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace Form_test
+{
+    internal class TestButton : Button
+    {
+        public TestButton(Point position, Size size, string  text)
+        {
+
+            Location = position;
+            Size = size;
+            Text = text;
+
+
+            Click += ClickEvent;
+        }
+
+
+        private void ClickEvent(object sender, EventArgs e)
+        {
+            MessageBox.Show("こんにちは");
+        }
+
+    }
+}
